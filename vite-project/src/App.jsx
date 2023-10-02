792f8d52e4c4a8e87a6ef7fb4c378459192ad166
abdurakhmonovsharif <fullstackdeveloper4413@gmail.com>
2023-10-02 22:10:14 +0500

    app jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,7 +4,7 @@ import Body from "./components/Body";
 import Footer from "./components/Footer";
 const App = () => {
     return (
-        // said 
+        // said1
         <React.Fragment>
             <Header />
             <Body />
